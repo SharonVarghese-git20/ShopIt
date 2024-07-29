73ff97b0ec0fdc4d60358e7598921fe9e25766b8
Sharon Varghese <varghesesharon423@gmail.com>
2024-07-29 16:15:13 +0530

    changed auth

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,7 +4,7 @@ import products from "./data.js";
 
 const seedProducts= async ()=>{
     try{
-        await mongoose.connect("mongodb+srv://ShopIt:101010@cluster0.xxfcwwg.mongodb.net/shopit?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("");
 
         await Product.deleteMany();
         console.log("Products Deleted");
